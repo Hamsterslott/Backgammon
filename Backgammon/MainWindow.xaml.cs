829f8b757b636a75d550c22b7f8ad95cf2aeab14
Hamsterslott <johanrasmussen94@gmail.com>
2016-02-08 22:16:40 +0100

    RIKTIGT OFFICIAL FINAL PUSH!!!! WOOH!!!!!!!!!!

--- Backgammon/MainWindow.xaml.cs
+++ Backgammon/MainWindow.xaml.cs
@@ -70,8 +70,8 @@ namespace Backgammon
 
 		private void init()
 		{
-			//gameBoard = _model.newGame();
-			gameBoard = _model.endGame();
+			gameBoard = _model.newGame();
+			//gameBoard = _model.endGame();
 			//gameBoard = _model.highStack();
 			//gameBoard = _model.bricksInMiddle();
 
